e07e01b6fa569ebc04b3b37563b4b39b0e2a877d
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-09-30 21:24:49 -0400

    Added Unit Test project and LibVLCSharp API

---
+++ testUnBound_MediaMate/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testUnBound_MediaMate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("testUnBound_MediaMate")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bc39355-cd21-4f49-ae77-53911ef2679e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
