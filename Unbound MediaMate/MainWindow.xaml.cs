f3b5fbfb285fb0dd5d9da864e1a9d37168362f50
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-10-01 20:05:26 -0400

    Made many changes. Created the constructor. Added everything needed to work with vlc's library

--- Unbound MediaMate/MainWindow.xaml.cs
+++ Unbound MediaMate/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using MediaPlayer = LibVLCSharp.Shared.MediaPlayer; // Using vlc's Media Player Class
 
 namespace Unbound_MediaMate
 {
@@ -20,9 +24,30 @@ namespace Unbound_MediaMate
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly LibVLC _vlcLibrary; // Required for library operations. Initializes VLC Engine.
+        private readonly MediaPlayer _mediaPlayer; // Provides media playback functionalities
+        private bool isMediaPlayerPlaying = false;
+        private bool isUserDraggingSlider = false;
+        const int kOne = 1;
         public MainWindow()
         {
             InitializeComponent();
+
+            // Initializing LibVLCSharp
+            Core.Initialize();
+
+            // Create new LibVLC instance
+            _vlcLibrary = new LibVLC();
+
+            // Create new MediaPlayer instance
+            _mediaPlayer = new MediaPlayer(_vlcLibrary);
+
+            videoView.MediaPlayer = _mediaPlayer; // Connecting MediaPlayer logic to VideoView visual component
+
+            DispatcherTimer timer = new DispatcherTimer(); // Initializing timer
+            timer.Interval = TimeSpan.FromSeconds(kOne); // Timer interval is 1 second
+            timer.Tick += timer_Tick; // Will create this method
+            timer.Start();
         }
     }
 }
